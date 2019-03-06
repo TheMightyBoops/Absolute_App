3ea4333db2a9475b016645a1c4444d5a22afae21
Lucas Nolting <lnolting@my.wctc.edu>
2019-03-05 19:48:12 -0600

    inital commit

---
+++ AbsoloteApp/MainPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AbsoloteApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            /*AbsoluteLayout.SetLayoutBounds(player, 
+                                           new Rectangle 
+                                           (1, 2, 
+                                                  AbsoluteLayout.AutoSize, 
+                                                  AbsoluteLayout.AutoSize));
+            AbsoluteLayout.SetLayoutBounds(computer,
+                                           new Rectangle
+                                           (3, 4,
+                                                  AbsoluteLayout.AutoSize,
+                                                  AbsoluteLayout.AutoSize));
+
+            AbsoluteLayout.SetLayoutBounds(counter,
+                                           new Rectangle
+                                           (7, 8,
+                                                  AbsoluteLayout.AutoSize,
+                                                  AbsoluteLayout.AutoSize));
+                                                  */
+
+        }
+    }
+}
